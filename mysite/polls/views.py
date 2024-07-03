6b162e587e5ec831a2c76ada0fbebadd6013970b
Ayushi Gupta <aayushi.gupta0405@gmail.com>
2024-07-03 20:04:55 +0530

    updated

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 
 from .models import Choice,Question
 class IndexView(generic.ListView):
