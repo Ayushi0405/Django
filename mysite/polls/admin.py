465f7d04ae2c92d446ce4a753e099d84cd14bacb
Ayushi Gupta <aayushi.gupta0405@gmail.com>
2024-07-07 20:04:55 +0530

    updated

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,6 +5,17 @@ from django.contrib import admin
 from .models import Question
 
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fields = ["pub_date", "question_text"]
 
